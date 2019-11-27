95e4655908233529f6a7ea8fc6f8ed7f13e190e4
Muhammad Bilal <mbj786.bilal@gmail.com>
2019-11-28 00:45:55 +0500

    Code reviewed

--- src/Components/Playlist.jsx
+++ src/Components/Playlist.jsx
@@ -6,47 +6,35 @@ import Divider from './Divider';
 import AudioSingleton from '../Audio/Audio'
 
 
-class Playlist extends Component{
-   
-    
-        
+class Playlist extends Component {
     state = {
         selectedSongs: [],
     }
-    
 
-        
-    
-    
-    handleOnClick=event=>{
-        const {selectedSongs}=this.state;
-        const audio = AudioSingleton.getInstance();
-        let currentTarget = event.currentTarget;
-        let songIndex = Number(currentTarget.getAttribute('song-index'));
-       audio.srcObject=selectedSongs[songIndex];
-       audio.play();
-        
-
-
-       
-        {console.log(selectedSongs)};   
-      
+    handleOnClick = song => {
+        const reader = new FileReader();
+        reader.readAsDataURL(song);
+        reader.onload = () => {
+            const audioPlayer = AudioSingleton.getInstance();
+            audioPlayer.setSong(reader.result);
+            audioPlayer.playSong();
+        }
     }
-  
-    onSongsSelect = event => {
-        let files = Array.from(event.target.files);
-        
-        this.setState({ selectedSongs: files });
-    }; 
 
-    render(){
-        const {selectedSongs} = this.state;
-        return(
+    onSongsSelect = event => {
+        const files = Array.from(event.target.files);
+
+        this.setState({ selectedSongs: files });
+    };
+
+    render() {
+        const { selectedSongs } = this.state;
+        return (
             <div className="playlist">
-                <Addsong onSongsSelect={this.onSongsSelect}/>
-                <Divider/>
-                { selectedSongs.length > 0 && <Songlist songs={selectedSongs} onClick={this.handleOnClick}/> }
-                
+                <Addsong onSongsSelect={this.onSongsSelect} />
+                <Divider />
+                {selectedSongs.length > 0 && <Songlist songs={selectedSongs} onClick={this.handleOnClick} />}
+
             </div>
         );
     }
